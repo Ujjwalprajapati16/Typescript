907cbe2848b82dd3f35b89544e4c9a729bc1ee9e
Ujjwal Prajapati <ujjwalprajapati154@gmail.com>
2024-12-11 17:59:59 +0530

    Assertions

---
+++ 08_Assertions/app.js
@@ -0,0 +1,9 @@
+"use strict";
+// Type Assertion
+// Type casting
+// Non-null assertion operator
+// Type assertion
+let val = 12;
+val.toFixed(2);
+// Non-null assertion operator
+val.toFixed(2);
---
+++ 08_Assertions/app.ts
@@ -0,0 +1,10 @@
+// Type Assertion
+// Type casting
+// Non-null assertion operator
+
+// Type assertion
+let val: any = 12;
+(val as Number).toFixed(2);
+
+// Non-null assertion operator
+val!.toFixed(2);
